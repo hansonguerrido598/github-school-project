05bbfb52dcf2415b884b2919dd17a0436b951160
hansonguerrido598 <hansonguerrido598@gmail.com>
2025-03-08 06:44:03 +0000

    Add 3.c

---
+++ src/3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+    if (x > 3) {
+        printf("The number is greater than 3\n");
+    } else {
+        printf("The number is not greater than 3\n");
+    }
+    return 0;
+}
