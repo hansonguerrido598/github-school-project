27e22a059f86feff1c527269f7816175f658380a
hansonguerrido598 <hansonguerrido598@gmail.com>
2025-03-10 03:17:44 +0000

    Add 5.c

---
+++ src/5.c
@@ -0,0 +1,13 @@
+  #include <stdio.h>
+  #include <stdlib.h>
+  
+  int main() {
+      int age = 25;
+      if (age >= 18) {
+          printf("You are eligible to vote.\n");
+      } else {
+          printf("You are not eligible to vote.\n");
+      }
+  
+      return 0;
+  }
