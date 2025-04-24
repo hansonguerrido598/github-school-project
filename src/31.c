f835cda22153b45314200c7e7f212f78cdeb66fd
hansonguerrido598 <hansonguerrido598@gmail.com>
2025-04-24 15:16:36 +0000

    Add 31.c

---
+++ src/31.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    int num = 5;
+    if (num > 0) {
+        printf("The number is positive.\n");
+    } else {
+        printf("The number is not positive.\n");
+    }
+    
+    return 0;
+}
