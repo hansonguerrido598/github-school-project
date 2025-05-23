d446bdd38623ad9c84d73e04fd11bfc6f05dd976
hansonguerrido598 <hansonguerrido598@gmail.com>
2025-05-23 05:56:03 +0000

    Add 37.c

---
+++ src/37.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int x = 5;
+    if (x > 0) {
+        printf("The number is positive.");
+    } else {
+        printf("The number is not positive.");
+    }
+    return 0;
+}
